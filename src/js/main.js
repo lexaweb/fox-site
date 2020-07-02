fd8e6eb88b5dfab87f01b57ac331640a0d51d5eb
Aleksey Stadnik <alekseystadnik@Alekseys-iMac.local>
2020-07-02 18:14:09 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,20 @@
+import $ from "jquery";
+import '@fancyapps/fancybox';
+import Swiper from "swiper";
+
+$(document).ready(() => {
+
+
+  var mySwiper = new Swiper('.swiper-container', {
+    speed: 400,
+    spaceBetween: 100,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+});
