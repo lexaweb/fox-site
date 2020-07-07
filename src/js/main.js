9086b3825db431f0bc2156034af2e32d5e7ed09f
Aleksey Stadnik <alekseystadnik@Alekseys-iMac.local>
2020-07-07 15:59:36 +0300

    удалил ненужные файлы

--- src/js/main.js
+++ src/js/main.js
@@ -2,30 +2,11 @@ import $ from "jquery";
 import "@fancyapps/fancybox";
 import Swiper from "swiper";
 
-$(document).ready(() => {
-  var mySwiper = new Swiper(".swiper-container", {
-    speed: 400,
-    spaceBetween: 100,
-    pagination: {
-      el: ".swiper-pagination",
-      type: "bullets",
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
-});
-
 $(document).ready(function () {
   $(".hamburger").click(function () {
     $(this).toggleClass("active");
     $(".header-menu-all").toggleClass("active");
     $(".hamburger").css("right", 120 + $(".header-menu-all").width());
-  });
-});
-$(document).ready(function () {
-  $(".hamburger").click(function () {
     if (!$(this).hasClass("active")) {
       $(this).css("right", 0);
     }
